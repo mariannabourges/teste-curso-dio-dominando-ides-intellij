5888a6caf2d50252d8af93abb8c459e1ea785e3a
Marianna <marianna.borgesdesouza@gmail.com>
2021-09-13 02:38:29 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) { //escreve sout
+
+        //CTRL + Y deleta linha
+        //CTRL + SHIFT + / comenta um bloco de código selecionado
+        //CTRL + / comenta linha por linha
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello, World! " + (a+b));*/
+        //CTRL + ALT + L organiza o código
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+        //CTRL + D duplica a linha
+        System.out.println(gato);
+
+    }
+}
+
